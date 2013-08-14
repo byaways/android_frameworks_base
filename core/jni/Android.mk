2aab81257841c38edfe8684164facb2470915839
Faryaab Sheikh <faryaabsheikh@gmail.com>
2013-08-14 06:15:35 +0500

    Fix  vectorization of Bitmap operation

--- core/jni/Android.mk
+++ core/jni/Android.mk
@@ -233,26 +233,6 @@ else
 		android/graphics/Bitmap.cpp
 endif
 
-ifeq ($(TARGET_ARCH), arm)
-  ifeq ($(TARGET_USE_KRAIT_BIONIC_OPTIMIZATION), true)
-    TARGET_arm_CFLAGS += -DUSE_NEON_BITMAP_OPTS -mvectorize-with-neon-quad
-    LOCAL_SRC_FILES+= \
-		android/graphics/Bitmap.cpp.arm
-  else
-    ifeq ($(TARGET_ARCH_VARIANT_CPU), cortex-a15)
-      TARGET_arm_CFLAGS += -DUSE_NEON_BITMAP_OPTS -mvectorize-with-neon-quad
-      LOCAL_SRC_FILES+= \
-		android/graphics/Bitmap.cpp.arm
-    else
-      LOCAL_SRC_FILES+= \
-		android/graphics/Bitmap.cpp
-    endif
-  endif
-else
-    LOCAL_SRC_FILES+= \
-		android/graphics/Bitmap.cpp
-endif
-
 ifeq ($(USE_OPENGL_RENDERER),true)
 	LOCAL_SHARED_LIBRARIES += libhwui
 endif
